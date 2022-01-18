8ec057bfff3763f9c92b025ce52115af8f1adb1c
Yvonne Morée <92443695+vonnesetcetera@users.noreply.github.com>
2022-01-18 12:26:12 +0100

    Add files via upload

---
+++ jQuery-Tabs-Start/script.js
@@ -0,0 +1,16 @@
+(function(){
+    'use strict';
+    $('#tabs > ul > li > a').click(function(){
+			$('#tabs > ul > li > a').css({'background': '#A2A2A2', 'color': "#CECECE"});
+			$(this).css({'background': "#EAEAEA", 'color': '#333'});
+
+			const thisTab = $(this).attr('href');
+			//alert(thisTab); check!
+			$('#tabs > div:visible').fadeOut(200, function(){
+				$(thisTab).fadeIn(200);
+
+			});
+
+		});
+		
+})();
