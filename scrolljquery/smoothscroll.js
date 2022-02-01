46113b64bc055e389e2e10dcd44c7c2a34e4232a
Yvonne Morée <92443695+vonnesetcetera@users.noreply.github.com>
2022-02-01 14:27:28 +0100

    Add files via upload
    interactivity with JS and jQuery, coursera course by Bill Mead, UC Davis.
    week 02 smoothscroll with jQuery

---
+++ scrolljquery/smoothscroll.js
@@ -0,0 +1,78 @@
+$('nav ul li a').click(function(){
+
+    var thisSection = $(this).attr('href');
+    var thisLink = $(this);
+
+    $('html, body').stop().animate({
+
+        scrollTop: $(thisSection).offset().top -200
+
+    }, 800, 'easeOutCirc');
+        
+    return false;
+
+});//end clickhandler
+
+$(window).on( 'load', function(){
+    var allLinks = $('nav ul li a');
+    var posts = $('section');
+    var pageTop;
+    var postPos;
+    var counter = 0;
+    var prevCounter = 0;
+    var doneResizing;
+
+    var postTops = [];
+
+    resetPagePosition();
+    //console.log(postTops);
+
+   $(window).scroll(function(){
+        pageTop = $(window).scrollTop() + 210;
+
+        if(pageTop > postTops[counter+1]) {
+            counter++;
+            console.log(`scrolling down ${counter}`);
+        }
+        else if (counter > 0 && pageTop < postTops[counter]) {
+            counter--;
+            console.log(`scrolling up ${counter}`);
+        }
+        if( counter != prevCounter) {
+            $(allLinks).removeAttr('class');
+            $('nav ul li a').eq(counter).addClass('selected');
+            prevCounter = counter;
+        }
+
+    });//end window scroll function
+
+    $(window).on('resize', function(){
+
+        clearTimeout(doneResizing);
+        doneResizing = setTimeout(function(){
+
+            resetPagePosition();
+        }, 500);
+   
+    });
+
+    function resetPagePosition() {
+        postTops = [];
+        posts.each(function () {
+            postTops.push(Math.floor($(this).offset().top));
+        });
+        var pagePosition = $(window).scrollTop() + 210;
+        counter = 0;
+
+        for (let i = 0; i < postTops.length; i++) {
+            if (pagePosition > postTops[i]) {
+                counter++;
+            }
+        }
+        counter--;
+        $(allLinks).removeAttr('class');
+        $('nav ul li a').eq(counter).addClass('selected');
+    }
+
+});//end window on  load function
+
